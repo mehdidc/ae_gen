b04a250a36d7085e039bb7b9b6ef6a4eea4adc83
Mehdi Cherti <mehdicherti@gmail.com>
2023-12-30 00:59:30 +0100

    delete

--- download_models.sh
+++
@@ -1,6 +0,0 @@
-if [[ ! -f convae.th ]]; then
-    wget https://github.com/mehdidc/ae_gen/releases/download/0.1.0/convae.th
-fi
-if [[ ! -f deep_convae.th ]]; then
-wget https://github.com/mehdidc/ae_gen/releases/download/0.1.0/deep_convae.th
-fi
